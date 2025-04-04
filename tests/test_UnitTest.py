74ed2e55a489dd344ef1a3ca01de6df77f73f9cf
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-04-04 16:43:11 +0300

    Update tests file for both codes

--- tests/test_UnitTest.py
+++ tests/test_UnitTest.py
@@ -1,70 +1,72 @@
 import unittest
 from ArrayList import ArrayList
 
-class TestArrayList(unittest.TestCase):
+class TestList(unittest.TestCase):
     def setUp(self):
-        self.list = ArrayList()
+        # Використовуємо змінну для вибору потрібної реалізації
+        self.list = ArrayList()  # Можна змінити на CircularLinkedList для тесту іншої реалізації
 
-    def test_append(self):
+    def test_append_and_length(self):
+        self.assertEqual(self.list.length(), 0)
         self.list.append('a')
-        self.assertEqual(self.list.length(), 1)
+        self.list.append('b')
+        self.assertEqual(self.list.length(), 2)
 
-    def test_insert(self):
+    def test_insert_and_get(self):
         self.list.append('a')
+        self.list.append('c')
         self.list.insert('b', 1)
+        self.assertEqual(self.list.get(0), 'a')
         self.assertEqual(self.list.get(1), 'b')
-        with self.assertRaises(IndexError):
-            self.list.insert('c', 5)
+        self.assertEqual(self.list.get(2), 'c')
 
     def test_delete(self):
         self.list.append('a')
         self.list.append('b')
         self.assertEqual(self.list.delete(0), 'a')
-        with self.assertRaises(IndexError):
-            self.list.delete(2)
+        self.assertEqual(self.list.length(), 1)
 
     def test_deleteAll(self):
-        self.list.extend(['a', 'b', 'a', 'c'])
-        self.list.deleteAll('a')
-        self.assertEqual(self.list._data, ['b', 'c'])
-
-    def test_get(self):
         self.list.append('x')
-        self.assertEqual(self.list.get(0), 'x')
-        with self.assertRaises(IndexError):
-            self.list.get(5)
+        self.list.append('y')
+        self.list.append('x')
+        self.list.deleteAll('x')
+        self.assertEqual(self.list.length(), 1)
+        self.assertEqual(self.list.get(0), 'y')
 
-    def test_clone(self):
+    def test_clone_and_reverse(self):
         self.list.append('a')
-        cloned = self.list.clone()
-        self.assertEqual(cloned.get(0), 'a')
-        cloned.append('b')
-        self.assertNotEqual(self.list.length(), cloned.length())
-
-    def test_reverse(self):
-        self.list.extend(['a', 'b', 'c'])
+        self.list.append('b')
+        self.list.append('c')
+        clone = self.list.clone()
+        self.assertEqual(clone.length(), 3)
         self.list.reverse()
-        self.assertEqual(self.list._data, ['c', 'b', 'a'])
+        self.assertEqual(self.list.get(0), 'c')
+        self.assertEqual(self.list.get(1), 'b')
+        self.assertEqual(self.list.get(2), 'a')
 
-    def test_findFirst(self):
-        self.list.extend(['a', 'b', 'a'])
+    def test_find(self):
+        self.list.append('a')
+        self.list.append('b')
+        self.list.append('a')
         self.assertEqual(self.list.findFirst('a'), 0)
+        self.assertEqual(self.list.findLast('a'), 2)
         self.assertEqual(self.list.findFirst('z'), -1)
 
-    def test_findLast(self):
-        self.list.extend(['a', 'b', 'a'])
-        self.assertEqual(self.list.findLast('a'), 2)
-        self.assertEqual(self.list.findLast('z'), -1)
-
     def test_clear(self):
-        self.list.append('x')
+        self.list.append('a')
+        self.list.append('b')
         self.list.clear()
         self.assertEqual(self.list.length(), 0)
 
-    def test_extend(self):
-        self.list.append('x')
-        self.list.extend(['y', 'z'])
-        self.assertEqual(self.list._data, ['x', 'y', 'z'])
+def test_extend(self):
+    self.list.append('a')
+    other = ArrayList()
+    other.append('b')
+    other.append('c')
+    self.list.extend(other._data) 
+    self.assertEqual(self.list.length(), 3)
+    self.assertEqual(self.list.get(2), 'c')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
